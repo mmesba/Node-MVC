28f85093e889f6f508fee810a80389c3646e739f
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-02-07 00:19:05 +0600

    index route configured with getTemplate helper

--- route.js
+++ route.js
@@ -5,6 +5,9 @@
  * Github link: https://github.com/mmesba
  * Date: 06/02/2022
  */
+
+const helpers = require("./helpers");
+
  
 // Dependencies.
  
@@ -12,9 +15,26 @@
 // App object or Module scaffolding.
 const routes = {} 
 // main functions or objects.
+ routes.index = (data, callback)=>{
+    //  Reject any request that is not a GET
+    if(data.method == 'get'){
+        // Read in a template as a string
+        helpers.getTemplate('index', (err, str)=>{
+            if(!err && str){
+                callback(200, str, 'html');
+            } else{
+                console.log(err);
+                callback(500, undefined, 'html');
+            }
+        })
+    } else{
+        callback(405, undefined, 'html');
+    }
+ }
  
- 
- 
+//  routes.test = (data, callback)=>{
+//      callback(200, {'test': 'ok'}, 'json');
+//  }
  
 // export the module.
  module.exports = routes; 
