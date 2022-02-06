0fd961446b4b71885730006b7f45deb68ce56ccf
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-02-06 20:54:27 +0600

    index file initiated

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Main File for Node MVC
+ * Description: Basic MVC server to deliver frontend data
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 06/02/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
