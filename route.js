8dbea1549851a25d88446bed8636dbbdec32f1da
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-02-06 23:46:06 +0600

    route file initiated

---
+++ route.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Routes
+ * Description: Apps routes
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 06/02/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+const routes = {} 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ module.exports = routes; 
