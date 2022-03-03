8f389f684c49fa7b71ee58535eca8a1a55a4fef3
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-03-04 00:25:54 +0600

    environment file initiated

---
+++ environment.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Environment Related Files
+ * Description: Environment Files for Project
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 04/03/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
