c0536daa4a48f111a9586c5e076da1832aae4368
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-03-05 13:05:59 +0600

    data file initiated

---
+++ data.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Data File
+ * Description: Data related file to deal with data.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 05/03/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
