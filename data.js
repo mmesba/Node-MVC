2ae6275242b02f4d0664d578eeff7045b2e98c66
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-03-05 21:46:13 +0600

    create data function added

--- data.js
+++ data.js
@@ -7,14 +7,44 @@
  */
  
 // Dependencies.
- 
+ const fs = require('fs');
+const path = require('path');
  
 // App object or Module scaffolding.
- 
+ const lib = {};
 // main functions or objects.
- 
+//  Base Directory of the data folder
+lib.baseDir =  path.join(__dirname, '/.data/');
+
+// Write data to file
+lib.create = (dir, file, data, callback)=>{
+    // open file for writing
+    fs.open(lib.baseDir+dir+'/'+file+'.json', 'wx' , (err1, fileDescriptor)=>{
+        if (!err1 && fileDescriptor) {
+            //  Convert data to string
+            const stringData = JSON.stringify(data);
+
+            // write data to file and then close it 
+            fs.writeFile(fileDescriptor, stringData, (err2)=>{
+                if (!err2) {
+                    fs.close(fileDescriptor, (err3)=>{
+                        if (!err3) {
+                            callback(false); 
+                          } else {
+                             callback('Error closing the new file !')
+                         }
+                    }) 
+                  } else {
+                     callback('Error writing to new file')
+                 }
+            })
+          } else {
+             callback('Could not create new file , it may already exist')
+         }
+    })
+}
  
  
  
 // export the module.
- 
+ module.exports = lib;
