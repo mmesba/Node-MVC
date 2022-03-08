55bce09f862c78dcd3acdc2e81ac240bd29e8b26
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-03-08 23:47:43 +0600

    user handling scaffolding added

--- users.js
+++ users.js
@@ -12,7 +12,26 @@
 // App object or Module scaffolding.
  const users = {};
 // main functions or objects.
- 
+
+// JSON API handler
+users.userHandler = (data, callback)=>{
+    // Check which method is is requested by user and response according to that
+    const acceptedMethods = ['get', 'post', 'put', 'delete'];
+    if(acceptedMethods.indexOf(data.method) > -1){
+        // Call the corresponding method defined below
+        users._users[data.method](data, callback);
+    } else{
+        callback(405)
+    }
+}
+
+
+ users._users = {};
+
+
+users._users.get = (data, callback)=>{
+    callback(200, {'ok': 'ok'});
+}
  
  
  
