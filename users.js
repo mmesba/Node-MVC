c2ab261706cc05e6112a2c88b61ea1baec6d2fe3
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-03-08 23:34:24 +0600

    user file initiated

---
+++ users.js
@@ -0,0 +1,20 @@
+/*
+ * Title: User Handler File
+ * Description: User Handler to Handle users
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 08/03/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ const users = {};
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ module.exports = users;
