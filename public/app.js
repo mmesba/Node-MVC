425dd4db014573007f268f3a6f787f7066b0ae04
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-04-16 23:51:47 +0600

    app js file initiated

---
+++ public/app.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Frontend Logic File
+ * Description: Frontend Logic file
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 16/04/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ const app = {};
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ module.exports = app;
